cfc966141f76d21f660d289eb81d40f7bfc88a27
xxxcoTbxxx <1vladkaniwets4@gmail.com>
2024-12-18 14:11:55 +0700

    adding taxes

--- test_calc.py
+++ test_calc.py
@@ -25,3 +25,9 @@ def test_negative_balance(calculator):
     """Тест отрицательного баланса."""
     calculator.add_expense(500)
     assert calculator.get_balance() == -500
+
+def test_add_tax(calculator):
+    calculator.add_income(1000)
+    calculator.add_tax(200)
+    assert calculator.get_balance() == 800
+
